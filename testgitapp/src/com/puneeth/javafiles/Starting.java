78f1cb4c6defe0d84784c0a77c478523c6cb81c2
sf <sf@sf-PC>
2017-01-28 12:54:08 +0530

    Added 1 sop puneeth's git..... changed

--- testgitapp/src/com/puneeth/javafiles/Starting.java
+++ testgitapp/src/com/puneeth/javafiles/Starting.java
@@ -7,6 +7,8 @@ public class Starting {
 		// Adding Comment to check for git changes for file in local repository
 		System.out.println("Starting to work with Git and GitHub");
 
+		
+		System.out.println("Puneeth's Git");
 	}
 
 }  
