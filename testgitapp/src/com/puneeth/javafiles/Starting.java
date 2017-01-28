362b53b5953a71b5e660b53eb45dbe826a46be21
puneeth shivalingaiah <puneeth.shivalingaiah@gmail.com>
2017-01-28 12:43:09 +0530

    Commiting testgitapp to local repository

---
+++ testgitapp/src/com/puneeth/javafiles/Starting.java
@@ -0,0 +1,12 @@
+package com.puneeth.javafiles;
+
+public class Starting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Starting to work with Git and GitHub");
+
+	}
+
+}
