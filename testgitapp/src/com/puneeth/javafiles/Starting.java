39f36ddadfbf66fed35b7d6ecaaf1aacbbae2bb3
sf22 <sf22@sf-PC>
2017-01-28 15:31:22 +0530

    22 added1 comment before main() method and added 2nd for loop

--- testgitapp/src/com/puneeth/javafiles/Starting.java
+++ testgitapp/src/com/puneeth/javafiles/Starting.java
@@ -1,6 +1,8 @@
 package com.puneeth.javafiles;
 
 public class Starting {
+	
+	// Addind Comment before main...  
 
 	public static void main(String[] args) {
 		
@@ -8,6 +10,11 @@ public class Starting {
 		System.out.println("Playinh around git");
 		for(int i=0; i<5; i++)
 			System.out.println(i);
+		
+		// Adding 1 more for loop
+		for(int x=10; x<15; x++);
+		
+		
 		// Adding Comment to check for git changes for file in local repository
 		System.out.println("Starting to work with Git and GitHub");
 
