af57ea871140c8a57ca128d65de23da579c6ff66
sf <sf@sf-PC>
2017-01-28 15:19:27 +0530

    added 1 sop playing around git added 1 simple for loop

--- testgitapp/src/com/puneeth/javafiles/Starting.java
+++ testgitapp/src/com/puneeth/javafiles/Starting.java
@@ -4,6 +4,10 @@ public class Starting {
 
 	public static void main(String[] args) {
 		
+		
+		System.out.println("Playinh around git");
+		for(int i=0; i<5; i++)
+			System.out.println(i);
 		// Adding Comment to check for git changes for file in local repository
 		System.out.println("Starting to work with Git and GitHub");
 
